bca77ddd2408ec626150320dfdb7b75a88c025c6
Henrique Torquato <henriquet@take.net>
2020-10-11 17:17:16 -0300

    Finish noise landscape

--- src/scenes/noise_landscape.js
+++ src/scenes/noise_landscape.js
@@ -1,5 +1,3 @@
-import CameraPanel3D from '../entities/camera_panel_3d'
-
 const LANDSCAPE_PANNEL_WIDTH = 50
 const LANDSCAPE_PANNEL_HEIGHT = 50
 const LANDSCAPE_WIDTH = 20
@@ -7,24 +5,21 @@ const LANDSCAPE_HEIGHT = 20
 
 export default class NoiseLandscape {
 
+    // renderer for 3D environments
     renderer = WEBGL
 
     setup() {
-        this.cameraPanel = new CameraPanel3D()
-        this.cameraPanel.setup()
-
         this.landscape = this.createLanscape()
-        console.log(this.landscape)
+        this.camera = new CircularCamera(
+            LANDSCAPE_WIDTH * LANDSCAPE_PANNEL_WIDTH,
+            LANDSCAPE_HEIGHT * LANDSCAPE_PANNEL_HEIGHT)
     }
 
     draw() {
         clear()
-        this.cameraPanel.draw()
+        background(118, 187, 207)
         this.drawLandscape()
-        // rotateX(frameCount * 0.01)
-        // rotateZ(frameCount * 0.01)
-
-        // camera(0, 0, 50, 0, 0, 0, 0, 1, 0)
+        this.camera.update()
     }
 
     createLanscape() {
@@ -32,7 +27,6 @@ export default class NoiseLandscape {
         * Width and Height are multiplied by two,
         * because each pannel dimention is made of two points.
         */
-       
         let landscape = []
         let xoff = 0
         for (let x = 0; x < LANDSCAPE_WIDTH * 2; x++) {
@@ -40,54 +34,70 @@ export default class NoiseLandscape {
             landscape[x] = new Array(LANDSCAPE_HEIGHT)
 
             for (let y = 0; y < LANDSCAPE_HEIGHT * 2; y++) {
-                const z = map(noise(xoff, yoff), 0, 1, 0, 10)
+                const z = map(noise(xoff, yoff), 0, 1, 0, 100)
                 landscape[x][y] = z
-                yoff += 0.01
+                yoff += 0.25
             }
 
-            xoff += 0.01
+            xoff += 0.25
         }
 
         return landscape
     }
 
     drawLandscape() {
-        // White panel color
-        fill(255, 255, 255)
-
-        // Draw vertical lines
-        for (let x = 0; x < LANDSCAPE_WIDTH + 1; x++) {
-            for (let y = 1; y < LANDSCAPE_HEIGHT + 1; y++) {
-                const p1 = this.getLandscapeValue(x, y-1)
-                const p2 = this.getLandscapeValue(x, y)
-
+        fill(52, 158, 93)
+        for (let x = 0; x < LANDSCAPE_WIDTH; x += 1) {
+            for (let y = 0; y < LANDSCAPE_HEIGHT; y += 1) {
                 beginShape(LINES)
-                vertex(p1.x, p1.y, p1.z)
-                vertex(p2.x, p2.y, p2.z)
-                endShape()
-            }
-        }
-
-        // Draw horizontal lines
-        for (let y = 0; y < LANDSCAPE_HEIGHT + 1; y++) {
-            for (let x = 1; x < LANDSCAPE_WIDTH + 1; x++) {
-                const p1 = this.getLandscapeValue(x-1, y)
-                const p2 = this.getLandscapeValue(x, y)
-
-                beginShape(LINES)
-                vertex(p1.x, p1.y, p1.z)
-                vertex(p2.x, p2.y, p2.z)
-                endShape()
+                this.drawVertex(x, y)
+                this.drawVertex(x + 1, y)
+                this.drawVertex(x + 1, y + 1)
+                this.drawVertex(x, y + 1)
+                endShape(CLOSE)
             }
         }
     }
 
-    getLandscapeValue(x, y) {
-        return {
-            x: x * LANDSCAPE_PANNEL_WIDTH,
-            y: y * LANDSCAPE_PANNEL_HEIGHT,
-            z: this.landscape[x][y]
+    drawVertex(x, y) {
+        const z = this.landscape[x][y]
+        vertex(x * LANDSCAPE_PANNEL_WIDTH, y * LANDSCAPE_PANNEL_HEIGHT, z)
+    }
+
+}
+
+const CAMERA_DISTANCE = 500
+const CAMERA_STEP = 0.01
+
+// Camera that rotates around a point
+class CircularCamera {
+
+    constructor(width, height) {
+        // start at angle 0
+        this.angle = 0
+
+        // sum the distance of the camera from the border
+        this.radius = (width / 2) + CAMERA_DISTANCE
+        this.center = {
+            x: width / 2,
+            y: height / 2
         }
+    }
+
+    update() {
+        /*
+        x(angle) = radius * cos(angle) + centerX
+        y(angle) = radius * sin(angle) + centerY
+        */
+        const x = this.radius * Math.cos(this.angle) + this.center.x
+        const y = this.radius * Math.sin(this.angle) + this.center.y
+
+        camera(
+            x, y, CAMERA_DISTANCE,
+            this.center.x, this.center.y, 0,
+            0, 0, -1)
+
+        this.angle = this.angle < 360 ? this.angle + CAMERA_STEP : 0
     }
 
 }
