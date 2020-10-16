7904b28d3d3051e77207b7dc9afbc6bc24918139
Henrique Torquato <henriquet@take.net>
2020-10-15 21:11:11 -0300

    Rename CircularCamera to RotatingCamera

--- src/entities/rotating_camera.js
+++ src/entities/rotating_camera.js
@@ -1,5 +1,5 @@
 // Camera that rotates around a point
-export default class CircularCamera {
+export default class RotatingCamera {
 
     constructor(center, distance, step) {
         this.distance = distance
@@ -22,7 +22,7 @@ export default class CircularCamera {
         const y = this.radius * Math.sin(this.angle) + this.center.y
 
         camera(
-            x, y, this.distance,
+            x, y, this.center.z,
             this.center.x, this.center.y, this.center.z,
             0, 0, -1)
 
