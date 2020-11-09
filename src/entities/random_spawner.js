fb8dcf3d5680d83329dfa97996e9653a1c70157d
Henrique Torquato <henriquet@take.net>
2020-11-09 20:49:28 -0300

    FIx random spawner stacking spawn calls

--- src/entities/random_spawner.js
+++ src/entities/random_spawner.js
@@ -1,21 +1,31 @@
 export default class RandomSpawner {
 
-    constructor(chance, period) {
+    constructor(chance, period, callback) {
         this.chance = chance
         this.period = period
+        this.callback = callback
+
+        this.time = this.nextTime()
     }
 
-    start(callback) {
-        this.callback = callback
-        setInterval(() =>
+    check() {
+        if (this.shouldRun())
         {
-            if (this.rollDice) callback()
-        },
-        this.period)
+            if (this.rollDice()) this.callback()
+            this.time = this.nextTime()
+        }
     }
 
     rollDice() {
         return Math.random() < this.chance
+    }
+
+    nextTime() {
+        return Date.now() + this.period
+    }
+
+    shouldRun() {
+        return Date.now() > this.time
     }
 
 }
